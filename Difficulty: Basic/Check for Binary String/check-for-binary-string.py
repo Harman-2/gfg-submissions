e31c5a88d879e47dfd4393dd74b103e3ab9cdc21
Harmanpreet Kaur <149608488+Harman-2@users.noreply.github.com>
2026-08-09 21:42:54 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Binary String/check-for-binary-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isBinary(self, s):
+        # code here
+        for char in s:
+            if char != '0' and char != '1':
+                return False
+        return True
+        
