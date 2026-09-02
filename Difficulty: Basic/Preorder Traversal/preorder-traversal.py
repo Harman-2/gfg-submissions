2071def15364a5af429f21e794da608b31c5b2ec
Harmanpreet Kaur <149608488+Harman-2@users.noreply.github.com>
2026-09-02 13:47:48 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.py
@@ -0,0 +1,22 @@
+'''Structure of Tree Node
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def preOrder(self, root):
+        res = []
+        
+        def traverse(root):
+            if not root:
+                return
+            res.append(root.data)
+            traverse(root.left)
+            traverse(root.right)
+        traverse(root)
+        return res
+    
+    
